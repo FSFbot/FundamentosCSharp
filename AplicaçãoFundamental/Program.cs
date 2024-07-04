199d24b940309852bcddb05db585392db8c43f16
FSFbot <felipe.sfreitas14@gmail.com>
2024-07-04 14:12:35 -0300

    Aplicação de um cronometro...

---
+++ AplicaçãoFundamental/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace AplicaçãoFundamental;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> arquivos = new List<int>();
+        
+        arquivos.Add(1);
+        arquivos.Add(3);
+        arquivos.Add(3);
+
+        foreach (int arquivo in arquivos)
+        {
+            Console.WriteLine(arquivo);
+        }
+        
+    }
+}
---
+++ AppTeste/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AppTeste;
+using System.Collections.Generic;
+
+class Program
+{
+   
+
+    static string chamadas(string nome, string sobrenome)
+    {
+      
+        return nome+" "+ sobrenome;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ola senhor "+""+chamadas("Felipe","Freitas"));
+        var valor = 0;
+        do
+        {
+            List<int> lista = new List<int>();
+            lista.Add(valor++);
+            Console.WriteLine(string.Join(", ",lista));
+        } while (valor <= 10);
+
+        
+    }
+}
