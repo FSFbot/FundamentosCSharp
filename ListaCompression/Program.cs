05f2bb0c81e891952e7d5004a49927d8c32fd227
FSFbot <felipe.sfreitas14@gmail.com>
2024-07-10 20:25:39 -0300

    Adição usando vscode

---
+++ ListaCompression/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListaCompression{
+    class Program{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola mundo em c#");
+            List<string> nomes = new List<string>();
+
+            nomes.Add("Felipe");
+            nomes.Add("Thamires");
+            nomes.Add("Laís");
+
+            foreach(var nome in nomes){
+                Console.WriteLine($"Olá Seja bem vindo {nome}");
+            }
+        }
+    }
+}
